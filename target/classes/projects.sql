4a843c0be63a2348b2324ad832bf9aa2cbe6e05a
sridharadg <sridharadg@gmail.com>
2023-06-25 20:49:13 +0530

    POM Added

---
+++ target/classes/projects.sql
@@ -0,0 +1,11 @@
+CREATE TABLE PROJECT2(
+    ID INT AUTO_INCREMENT,
+    NAME VARCHAR(255),
+    DESCRIPTION VARCHAR(255)
+);
+
+INSERT INTO PROJECT2 ( ID, NAME, DESCRIPTION)
+VALUES
+(1, 'java',	'Java Project'),
+(2, 'python',	'Python Project'),
+(3, '.net',	'.net Project');
